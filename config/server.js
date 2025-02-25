353ab4515d608d018d3dcb9d2a96e8f9515daf93
Eidrien2810 <138817501+Eidrien2810@users.noreply.github.com>
2025-02-25 14:48:59 -0400

    eorganiza la estructura del proyecto siguiendo la arquitectura MVC - Configura Express para servir archivos estáticos correctamente - Ajusta las rutas en `server.js` para apuntar a los directorios   correctos -Actualiza `.gitignore` para ignorar archivos y directorios     innecesarios - Añade configuración de `nodemon` para desarrollo - Actualiza dependencias en `package.json`

---
+++ config/server.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pc = require('picocolors');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+
+// Ajuste de la ruta para servir archivos estáticos
+app.use(express.static(path.join(__dirname, '../public')));
+
+const PORT = process.env.PORT ?? 3000;
+
+// Ajuste de las rutas para las vistas
+app.get('/', (req, res, next) => {
+  res.status(200).sendFile(path.join(__dirname, '../views', 'principal.html'));
+});
+
+app.get('/principal', (req, res, next) => {
+  res.status(200).sendFile(path.join(__dirname, '../views', 'principal.html'));
+});
+
+// modificar cuando JUAN me mande lo que le falta
+app.get('/productos', (req, res, next) => {
+  res.status(200).sendFile(path.join(__dirname, '../views', 'crear_productos.html'));
+});
+
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.status(404).send('<h1>404 not found</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(pc.magenta(`server listening on http://localhost:${PORT}`));
+  console.log(__dirname);
+});
